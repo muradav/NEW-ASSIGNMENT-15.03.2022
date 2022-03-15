e48bc940e7c32c4375703e6c51e647c1edef19fb
muradav <muradav@code.edu.az>
2022-03-15 05:21:39 -0700

    feat: StringBuilder'in yeniden ishlenib tekmilleshdirilmesi

---
+++ String.Builder.Type/String.Builder.Type/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace String.Builder.Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Reverse("CodeAcademy"));
+        }
+
+        static StringBuilder Reverse(string word)
+        {
+            StringBuilder newString = new StringBuilder();
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+                newString.Append(word[i]);
+            }
+            return newString;
+        }
+    }
+}
