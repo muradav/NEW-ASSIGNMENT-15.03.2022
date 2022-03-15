c091cf65ad1785bf581c2d49e30a81c695156809
muradav <muradav@code.edu.az>
2022-03-15 06:48:29 -0700

    feat: Task2.nin ishlenub hazirlanmasi

---
+++ Task2.15.03.2022/Task2.15.03.2022/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task2._15._03._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 3, -6, -5 };
+
+            int[] newArr = PositiveArray(ref numbers);
+            foreach (var item in newArr)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+
+        static int[] PositiveArray(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    arr[i] = -arr[i];
+
+                }
+                
+            }
+            return arr;
+        }
+    }
+}
