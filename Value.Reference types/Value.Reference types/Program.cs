971d7fa5f1c4353c30a7d32e05cdcb344a586eca
muradav <muradav@code.edu.az>
2022-03-15 02:33:21 -0700

    :feat: Value and Refernce types examples

---
+++ Value.Reference types/Value.Reference types/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Value.Reference_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 5;
+            //ChangeNum(num);
+            //Console.WriteLine($"outside: {num}");
+
+            int[] numbers = {1, 2, 3, 4};
+            ChangeIndex(numbers);
+            Console.WriteLine($"outside: {numbers[0]}");
+        }
+
+        static void ChangeNum(int n)
+        {
+            n = 10;
+            Console.WriteLine($"method: {n}");
+
+        }
+
+        static void ChangeIndex(int[] arr)
+        {
+            arr[0] = 10;
+            Console.WriteLine($"method: {arr[0]}");
+        }
+
+    }
+}
