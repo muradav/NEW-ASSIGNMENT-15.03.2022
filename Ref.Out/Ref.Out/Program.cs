4e212e734fd02a9b72eeb749cb730686978737c8
muradav <muradav@code.edu.az>
2022-03-15 02:43:26 -0700

    feat:ref/out mentiqinin yeniden ishlenib menimsenilmesi

---
+++ Ref.Out/Ref.Out/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ref.Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+             "ref" evvelceden deyer isteyir 
+             amma method daxilinde deyer menimsedilmesede olar
+                         
+             amma "out" tam eksine evvelceden deyer menimsedilmese de olar. 
+             esas method icinde deyer menimsedilmelidi
+             
+             */
+            
+                                   
+            
+            int num = 5;
+            //ChangeNum(ref num);
+            ChangeNum(out num);
+            Console.WriteLine($"outside {num}");
+        }
+
+        static void ChangeNum(out int n)
+        {
+            n = 10;
+            Console.WriteLine($"method {n}");
+        }
+    }
+}
