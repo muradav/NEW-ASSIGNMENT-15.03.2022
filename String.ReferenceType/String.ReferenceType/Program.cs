2eed594f1033d547b336354d8cdfbf82215c779e
muradav <muradav@code.edu.az>
2022-03-15 04:54:27 -0700

    feat: string reference type novlerinin ishlenmesi

---
+++ String.ReferenceType/String.ReferenceType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace String.ReferenceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello";
+            string word2 = word;
+            word2 = "hi";
+            //Console.WriteLine(word2);
+
+            string word3 = "hi";
+            Console.WriteLine(word2 == word3);
+
+
+        }
+
+
+    }
+}
