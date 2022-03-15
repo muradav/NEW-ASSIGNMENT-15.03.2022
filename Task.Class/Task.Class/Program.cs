df219639f4079b544c6535701b2961aea6777969
muradav <muradav@code.edu.az>
2022-03-15 01:16:58 -0700

    feat:sinifde ishlenen taskin yeniden ishlenmesi

---
+++ Task.Class/Task.Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task.Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("CodeAcademy"));
+            
+        }
+
+        static string Reverse(string word)
+        {
+            string newWord = String.Empty;
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+                newWord += (word[i]);
+            }
+            return newWord;
+        }
+
+       
+
+      
+       
+        
+        
+        
+    }
+}
